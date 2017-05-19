ecf82d2b8b520080782915534c3d175a096dae76
1111006621 <1111006621@LAB-B121-35.univille.br>
2017-05-18 22:12:21 -0300

    Q3 e implementacao da Classe ArrayStack

--- Exercicios1805/src/estd/Q3.java
+++ Exercicios1805/src/estd/Q3.java
@@ -2,10 +2,17 @@ package estd;
 
 public class Q3 {
 	public int[] toArray(Stack<Integer> stack) {
-		/*
-		 * Implementar
-		 */
 
-		return null;
+		int[] array = new int[stack.getSize()];
+		int x = 0;
+		while (!stack.isEmpty()) {
+			int aux = stack.pop();
+			if (aux % 2 == 0) {
+				array[x++] = aux;
+			}
+
+		}
+
+		return array;
 	}
 }
